8f1ea3c8dd956802c507b339787ea7bdf08a99bc
Mohylko Mykola <ljkepman@gmail.com>
2024-03-07 13:23:49 +0200

    __str__ function added to Articles

--- MyGIS/models.py
+++ MyGIS/models.py
@@ -7,3 +7,6 @@ class Articles(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.title
