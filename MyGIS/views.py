622f90e46b690c7fa9cfa90438b3f781dd5361c1
Mohylko Mykola <ljkepman@gmail.com>
2024-03-11 11:41:01 +0200

    Template base was rebuild added. Static file main.css was added.

--- MyGIS/views.py
+++ MyGIS/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
 
 def home(request):
     return render(request, 'MyGIS/home.html')
