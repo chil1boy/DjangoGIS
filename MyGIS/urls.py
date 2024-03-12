4b87705a6bc0b7e625762ea3f3a40684af327116
Mohylko Mykola <ljkepman@gmail.com>
2024-03-12 13:05:13 +0200

    Article url pattern, view and template were created

--- MyGIS/urls.py
+++ MyGIS/urls.py
@@ -4,4 +4,5 @@ from MyGIS import views
 
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
+    re_path(r'^articles/(?P<article_id>[0-9]+)/$', views.show_articles, name='article'),
 ]
--- MyGIS/views.py
+++ MyGIS/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from MyGIS.models import Articles
 
@@ -12,4 +12,9 @@ def home(request):
     context = {
         'articles': articles
     }
-    return render(request, 'mygis/home.html', context)
+    return render(request, 'MyGIS/home.html', context)
+
+
+def show_articles(request, article_id):
+    article = get_object_or_404(Articles, id=article_id)
+    return render(request, 'MyGIS/article.html', {'article': article})
